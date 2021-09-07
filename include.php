48f511521b762edf13a1892a63f53a63fe9ca9d8
Arowolo886 <78471747+Arowolo886@users.noreply.github.com>
2021-09-07 13:18:22 +0100

    created an html  file

---
+++ include.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+ <head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Arowolo">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Description" content="working on Git collaboration">
+ </head>
+ <body>
+   <div>  
+     <h3> Fill in this form</h3>
+      <form action="" method="POST">
+          <div class="form-group">
+            <label> Fullname </label>
+            <input type="text" name="name" id="name">
+          </div>
+        
+        
+          <div class="form-group">
+            <label> Username </label>
+            <input type="text" name="username" id="username">
+          </div>
+      
+        
+          <div class="form-group">
+            <label> Password </label>
+            <input type="text" name="pass" id="pass">
+          </div>
+        
+        
+          <div class="form-group">
+            <button name="reg">Register </button>
+          </div>
+      <form>
+        
+   </div>
+    
+ </body>
+</html>
